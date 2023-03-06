8fb14703ee6f7f8a8d5af0ea820cec8d889661ee
mlsyptrcia <113995276+mlsyptrcia@users.noreply.github.com>
2023-03-06 11:34:46 +0800

    Add files via upload

---
+++ avaragefilter.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('anjing.jpg')
+
+kernel_size = (5, 5)
+
+kernel = np.ones(kernel_size, np.float32) / (kernel_size[0] * kernel_size[1])
+
+output = cv2.filter2D(img, -2, kernel)
+
+cv2.imshow('Original Image', img)
+cv2.imshow('Filtered Image', output)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
